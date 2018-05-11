797f5031b473ee90c96bea8e2f25c67cfd7499c2
Mark Manalo <marksmanalo@gmail.com>
2018-05-11 15:29:08 -0700

    wrong files

--- backend/CustomerInfo/CustomerInfo/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace CustomerInfo.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- backend/CustomerInfo/CustomerInfo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CustomerInfo.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
